959bdb6b6aaaf5cb71cb7201a9370482c91c695e
Robert Toth-Ludanyi <roberttothmails@yahoo.com>
2024-06-23 10:59:12 +0200

    Setup finally seems correct

---
+++ src/components/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import '../styles/App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1 className="text-3xl font-bold">
+        Hello world!
+      </h1>
+    </>
+  )
+}
+
+export default App
