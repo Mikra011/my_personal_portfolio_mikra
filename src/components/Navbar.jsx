0fea4a5aadc4957c409037a1b453bd07e939692f
Robert Toth-Ludanyi <roberttothmails@yahoo.com>
2024-06-26 21:04:51 +0200

    Started to style navbar

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,10 @@
+// import React, { useState } from 'react'
+// import { Link } from 'react-router-dom'
+// import '../styles/index.css';
+
+// export default function Navbar ({ isLandingPage }) {
+//   return (
+
+//   )
+// }
+
