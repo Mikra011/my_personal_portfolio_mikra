cd25ec3b74034cb21fa7288f36d58b108cde0588
Robert Toth-Ludanyi <roberttothmails@yahoo.com>
2024-06-27 15:17:45 +0200

    Navbar placed in its own comp

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,57 +1,28 @@
 import React from 'react'
-import { Routes, Route, Link, useLocation } from 'react-router-dom'
+import { Routes, Route } from 'react-router-dom'
 import LandingPage from './Landingpage'
 import Home from './Home'
 import About from "./About"
 import Projects from "./Projects"
 import Contacts from "./Contacts"
+import Navbar from './Navbar'
 import '../styles/customstyles.css'
 import '../styles/index.css'
 
-function App() {
-  const location = useLocation()
-  const isLandingPage = location.pathname === '/'
+export default function App() {
 
-  return (
-    <div>
-
-      <nav className={isLandingPage ? 'hide-nav' : 'relative px-4 py-4 flex justify-between items-center'}>
-        <h1 className="text-lg md:text-xl lg:text-2xl font-bold leading-tight md:leading-none text-center md:text-left">
-          <span className="font-handwritten">Mikra Portfolio Site</span>
-        </h1>
-        <div className="lg:hidden">
-          <button className="navbar-burger flex items-center text-blue-600 p-3">
-            <svg className="block h-4 w-4 fill-current" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
-              <title>Mobile menu</title>
-              <path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z"></path>
-            </svg>
-          </button>
+    return (
+        <div>
+            <Navbar />
+            <Routes>
+                <Route path='/' element={<LandingPage />} />
+                <Route path='home' element={<Home />} />
+                <Route path='about' element={<About />} />
+                <Route path='projects' element={<Projects />} />
+                <Route path='contacts' element={<Contacts />} />
+            </Routes>
         </div>
-        <ul className="hidden lg:flex lg:items-center lg:space-x-6">
-          <li>
-            <Link to="/home" className="text-sm text-gray-400 hover:text-gray-500">Home</Link>
-          </li>
-          <li>
-            <Link to="/about" className="text-sm text-gray-400 hover:text-gray-500">About</Link>
-          </li>
-          <li>
-            <Link to="/projects" className="text-sm text-gray-400 hover:text-gray-500">Projects</Link>
-          </li>
-          <li>
-            <Link to="/contacts" className="text-sm text-gray-400 hover:text-gray-500">Contacts</Link>
-          </li>
-        </ul>
-      </nav>
-
-      <Routes>
-        <Route path='/' element={<LandingPage />} />
-        <Route path='home' element={<Home />} />
-        <Route path='about' element={<About />} />
-        <Route path='projects' element={<Projects />} />
-        <Route path='contacts' element={<Contacts />} />
-      </Routes>
-    </div>
-  )
+    )
 }
 
-export default App;
+
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,10 +1,45 @@
-// import React, { useState } from 'react'
-// import { Link } from 'react-router-dom'
-// import '../styles/index.css';
+// Navbar.js
+import React from 'react'
+import { Link, useLocation } from 'react-router-dom'
+import '../styles/customstyles.css'
+import '../styles/index.css'
 
-// export default function Navbar ({ isLandingPage }) {
-//   return (
+export default function Navbar() {
+  const location = useLocation()
+  const isLandingPage = location.pathname === '/'
 
-//   )
-// }
+  return (
+    <nav className={isLandingPage ? 'hide-nav' : 'relative px-4 py-4 flex justify-between items-center'}>
+      <h1 className="text-lg md:text-xl lg:text-2xl font-bold leading-tight md:leading-none text-center md:text-left">
+        <span className="font-handwritten">Mikra Portfolio Site</span>
+      </h1>
+      <div className="lg:hidden">
+        <button className="navbar-burger flex items-center text-blue-600 p-3">
+          <svg className="block h-4 w-4 fill-current" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+            <title>Mobile menu</title>
+            <path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z"></path>
+          </svg>
+        </button>
+      </div>
+      <ul className="hidden lg:flex lg:items-center lg:space-x-6">
+        <li>
+          <Link to="/home" className="text-sm text-gray-400 hover:text-gray-500">Home</Link>
+        </li>
+        <li>
+          <Link to="/about" className="text-sm text-gray-400 hover:text-gray-500">About</Link>
+        </li>
+        <li>
+          <Link to="/projects" className="text-sm text-gray-400 hover:text-gray-500">Projects</Link>
+        </li>
+        <li>
+          <Link to="/contacts" className="text-sm text-gray-400 hover:text-gray-500">Contacts</Link>
+        </li>
+      </ul>
+    </nav>
+  )
+}
+
+
+
+
 
