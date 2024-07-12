1d4b3e6ac9cae9a3714dc3012a44a8c146779987
Robert Toth-Ludanyi <roberttothmails@yahoo.com>
2024-07-12 21:49:54 +0200

    Worked on landing page text styles

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -4,6 +4,9 @@ export default function Home() {
     return (
         <div>
             <h2>This is the Home page</h2>
+            <h3>
+                Hi, I&apos;m Robert, a passionate and dedicated aspiring Full Stack Developer. I specialize in creating dynamic and responsive web applications using technologies like JavaScript, React, Node.js, and SQL. I&apos;m eager to bring my skills and enthusiasm to a forward-thinking development team.
+            </h3>
         </div>
     )
 }
--- src/components/Landingpage.jsx
+++ src/components/Landingpage.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import { useNavigate } from "react-router-dom"
+import '../styles/landingpage/arrow.css'
+import '../styles/landingpage/textanimation.css'
 
 export default function LandingPage() {
     const navigate = useNavigate()
@@ -8,10 +10,25 @@ export default function LandingPage() {
         navigate('home')
     }
     return (
-        <div className="laning-page">
-            <button onClick={routeToSite}>
-                Continue to Page
-            </button>
+        <div className="landing-page">
+            <section id='welcome-section' className=''>
+                <div className="text-container">
+                    <h1 className="animated-welcome">Welcome To My Portfolio</h1>
+                </div>
+                <button onClick={routeToSite}>
+                    Continue as Guest
+                </button>
+                <h1>Or</h1>
+                <button className="arrow-down-button">
+                    <div className="arrow-down">
+                        <div className="left"></div>
+                        <div className="right"></div>
+                    </div>
+                </button>
+            </section>
+            <section id='login-section'>
+
+            </section>
         </div>
     )
 }
