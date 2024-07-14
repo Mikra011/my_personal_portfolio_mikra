1529ff719326dcbe7f5f91ef38836152ad7631f5
Robert Toth-Ludanyi <roberttothmails@yahoo.com>
2024-07-14 15:05:22 +0200

    small adjustments

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -3,10 +3,9 @@ import React from "react"
 export default function Home() {
     return (
         <div>
-            <h2>This is the Home page</h2>
-            <h3>
+            <p>
                 Hi, I&apos;m Robert, a passionate and dedicated aspiring Full Stack Developer. I specialize in creating dynamic and responsive web applications using technologies like JavaScript, React, Node.js, and SQL. I&apos;m eager to bring my skills and enthusiasm to a forward-thinking development team.
-            </h3>
+            </p>
         </div>
     )
 }
