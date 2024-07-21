3bc5b725b886f9deea92fe6d8e9c97836c4d11c8
Robert Toth-Ludanyi <roberttothmails@yahoo.com>
2024-07-21 19:19:06 +0200

    wip

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -3,7 +3,7 @@ import React from "react"
 export default function Projects() {
     return (
         <div className="h-screen">
-            <h1>Here is the Projects page</h1>
+            <h1>Here is the Projects page!</h1>
         </div>
     )
 }
