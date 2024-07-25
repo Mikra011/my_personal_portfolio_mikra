7a538fe14e9f43c8ae403ddad586a4297e018809
Robert Toth-Ludanyi <roberttothmails@yahoo.com>
2024-07-25 15:58:17 +0200

    pictures are not ready yet

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -2,13 +2,18 @@ import React from "react"
 
 export default function Home() {
     return (
-        <div className="h-screen">
-            <h1 className="p-6">
-                Turning Ideas into Interactive Experiences.
-            </h1>
-            <p>
-                Hi, I&apos;m Robert, a passionate and dedicated aspiring Full Stack Developer. I specialize in creating dynamic and responsive web applications using technologies like JavaScript, React, Node.js, and SQL. I&apos;m eager to bring my skills and enthusiasm to a forward-thinking development team.
-            </p>
+        <div className="">
+            <div className="h-screen text-center ">
+                <h1 className="p-6 ">
+                    Turning Ideas into Interactive Experiences
+                </h1>
+            </div>
+            <div className="h-screen">
+                <p>
+                    Hi, I&apos;m Robert, a passionate and dedicated aspiring Full Stack Developer. I specialize in creating dynamic and responsive web applications using technologies like JavaScript, React, Node.js, and SQL. I&apos;m eager to bring my skills and enthusiasm to a forward-thinking development team.
+                </p>
+            </div>
         </div>
+
     )
 }
