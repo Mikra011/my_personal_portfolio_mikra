a77950a6f7b3bea06c0734210604f1199aba8945
Robert Toth-Ludanyi <roberttothmails@yahoo.com>
2024-07-26 14:07:05 +0200

    About section images are respinsive

--- src/components/ImageGrid.jsx
+++ src/components/ImageGrid.jsx
@@ -10,9 +10,11 @@ const ImageGrid = ({ images }) => {
     return (
         <div className="grid">
             {rows.map((row, rowIndex) => (
-                <div className="row" key={rowIndex}>
+                <div className="row h-24 sm:h-36 md:h-52 lg:h-60" key={rowIndex}>
                     {row.map((src, index) => (
-                        <img key={index} src={src} alt={`image-${rowIndex}-${index}`} />
+                        <img 
+                        key={index} src={src} 
+                        alt={`image-${rowIndex}-${index}`} />
                     ))}
                 </div>
             ))}
