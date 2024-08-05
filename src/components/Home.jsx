050103b14ef833c9b6401d18bcc568d6c533e4e8
Robert Toth-Ludanyi <roberttothmails@yahoo.com>
2024-08-05 14:26:48 +0200

    deleted decortext

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,6 +1,5 @@
 import React from "react"
 import RingText from "./RingText"
-import '../styles/decortext.css'
 
 export default function Home() {
     return (
@@ -19,7 +18,7 @@ export default function Home() {
                     <div className="w-60 md:w-96" >
                         <p className="
                         p-6 text-base md:text-xl text-left font-kurale
-                        rounded-xl border-2 border-scarlet-400 bg-spring-200 dark:bg-amethyst-800"
+                        rounded-xl bg-spring-200 dark:bg-amethyst-800"
                         >
                             I&apos;m Robert, a passionate and dedicated aspiring <span className="font-alfa">Full Stack Developer</span>. I specialize in creating dynamic and responsive web applications using technologies like <span className="font-alfa">JavaScript, React and Node.js</span>. I&apos;m eager to bring my skills and enthusiasm to a forward-thinking development team.
                         </p>
@@ -49,19 +48,6 @@ export default function Home() {
                 </div>
             </section>
 
-            {/* <section className="h-52 truncate mt-36 sm:mt-0">
-                <div className="grid-text">
-                    <div className="row-text font-alex text-lg sm:text-2xl">
-                        <span>Descipline Commitment Resiliance Passion Dedication Creativity Collaboration Precision Efficiency Integrity Descipline Commitment Resiliance Passion Dedication Creativity Collaboration Precision Efficiency Integrity  Descipline Commitment Resiliance Passion Dedication Creativity Collaboration Precision Efficiency Integrity</span>
-                    </div>
-                    <div className="row-text font-dancing text-2xl sm:text-4xl lg:text-6xl">
-                        <span>My Portfolio - My Portfolio - My Portfolio - My Portfolio - My Portfolio - My Portfolio - My Portfolio - My Portfolio - My Portfolio</span>
-                    </div>
-                    <div className="row-text font-alex text-lg sm:text-2xl">
-                        <span> Passion Dedication Creativity Collaboration Precision Efficiency Integrity Descipline Commitment Resiliance  Passion Dedication Creativity Collaboration Precision Efficiency Integrity Descipline Commitment Resiliance </span>
-                    </div>
-                </div>
-            </section> */}
         </div>
 
     )
