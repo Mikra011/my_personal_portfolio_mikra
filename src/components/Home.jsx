c09678cbf059b887011325115647f09494ea88be
Robert Toth-Ludanyi <roberttothmails@yahoo.com>
2024-08-05 14:17:12 +0200

    home page almost done

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -13,6 +13,7 @@ export default function Home() {
                     for your <span className="text-3xl">business</span>
                 </p>
             </section>
+
             <section className="max-h-150vh md:h-screen flex justify-center items-center">
                 <div className="flex-column sm:flex justify-between gap-x-10 items-center">
                     <div className="w-60 md:w-96" >
@@ -29,40 +30,38 @@ export default function Home() {
                 </div>
 
             </section>
-            <section className="h-52 overflow-hidden">
-                <div className="gridtext">
-                    <div className="rowtext font-alex">
-                        <span></span>
-                        <span></span>
-                        <span></span>
-                        <span></span>
-                        <span></span>
-                        <span></span>
-                        <span></span>
-                        <span></span>
-                        <span></span>
-                        <span></span>
-                    </div>
-                    <div className="rowtext font-dancing text-5xl">
-                        <span>My Portfolio - My Portfolio - My Portfolio - My Portfolio - My Portfolio - My Portfolio - My Portfolio - My Portfolio - My Portfolio </span>
-                    </div>
-                    <div className="rowtext font-alex">
-                        <span></span>
-                        <span></span>
-                        <span></span>
-                        <span></span>
-                        <span></span>
-                        <span></span>
-                        <span></span>
-                        <span></span>
-                        <span></span>
-                        <span></span>
-                    </div>
+
+            <section className="h-72 truncate mt-36 sm:mt-0 text-center content-center">
+                <div className="font-play text-2xl sm:text-4xl lg:text-6xl">
+                    <h1>My Portfolio</h1>
                 </div>
             </section>
-            <section className="h-screen">
 
+            <section className="h-screen flex flex-col justify-center items-center">
+                <div className="size-32 border-2 border-scarlet-400 rounded-xl bg-spring-200 dark:bg-amethyst-800">
+
+                </div>
+                <div className="size-32 border-2 border-scarlet-400 rounded-xl bg-spring-200 dark:bg-amethyst-800">
+
+                </div>
+                <div className="size-32 border-2 border-scarlet-400 rounded-xl bg-spring-200 dark:bg-amethyst-800">
+
+                </div>
             </section>
+
+            {/* <section className="h-52 truncate mt-36 sm:mt-0">
+                <div className="grid-text">
+                    <div className="row-text font-alex text-lg sm:text-2xl">
+                        <span>Descipline Commitment Resiliance Passion Dedication Creativity Collaboration Precision Efficiency Integrity Descipline Commitment Resiliance Passion Dedication Creativity Collaboration Precision Efficiency Integrity  Descipline Commitment Resiliance Passion Dedication Creativity Collaboration Precision Efficiency Integrity</span>
+                    </div>
+                    <div className="row-text font-dancing text-2xl sm:text-4xl lg:text-6xl">
+                        <span>My Portfolio - My Portfolio - My Portfolio - My Portfolio - My Portfolio - My Portfolio - My Portfolio - My Portfolio - My Portfolio</span>
+                    </div>
+                    <div className="row-text font-alex text-lg sm:text-2xl">
+                        <span> Passion Dedication Creativity Collaboration Precision Efficiency Integrity Descipline Commitment Resiliance  Passion Dedication Creativity Collaboration Precision Efficiency Integrity Descipline Commitment Resiliance </span>
+                    </div>
+                </div>
+            </section> */}
         </div>
 
     )
