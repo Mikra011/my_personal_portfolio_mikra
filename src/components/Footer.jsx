7734132fd40c994cfd1c41b86a1031393b357b90
Robert Toth-Ludanyi <roberttothmails@yahoo.com>
2024-08-09 18:54:55 +0200

    footer almost ready

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,13 +3,18 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 export default function Footer() {
     return (
-        <footer className="bottom-0 z-50 w-full flex flex-row justify-around p-6 border-2">
+        <footer className="bottom-0 z-50 w-full flex flex-row justify-between p-2">
             <div>
-                <FontAwesomeIcon icon={faSquareGithub} />
-                <FontAwesomeIcon icon={faLinkedin} />
+                <FontAwesomeIcon className='fa-2x md:fa-3x p-1' icon={faSquareGithub} />
+                <FontAwesomeIcon className='fa-2x md:fa-3x p-1' icon={faLinkedin} />
             </div>
-            <p className='font-alex text-sm'>Robert Toth-Ludanyi 2024</p>
-            <a href="" className='text-sm font-play font-medium'>Resume</a>
+            {/* <p className='font-kurale text-sm md:text-lg'>Robert Toth-Ludanyi 2024</p> */}
+            <a href="" className='
+            text-md font-play font-medium text-center border-2 p-2
+            rounded-full border-transparent
+            hover:border-oasis-600 hover:border-2'>
+                Resume
+            </a>
         </footer>
     )
 }
