61eb3d78ecce50a65d91c6a82f4ba05769bfdfd9
Robert Toth-Ludanyi <roberttothmails@yahoo.com>
2024-08-11 14:53:00 +0200

    bug fix

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,11 +5,14 @@ export default function Footer() {
     return (
         <footer className="bottom-0 z-50 w-full flex flex-row justify-between p-2">
             <div>
-                <FontAwesomeIcon className='fa-2x md:fa-3x p-1' icon={faSquareGithub} />
-                <FontAwesomeIcon className='fa-2x md:fa-3x p-1' icon={faLinkedin} />
+                <FontAwesomeIcon className='fa-2x md:fa-3x p-1 transition-transform duration-200 hover:scale-125' icon={faSquareGithub} />
+                <FontAwesomeIcon className='fa-2x md:fa-3x p-1 transition-transform duration-200 hover:scale-125' icon={faLinkedin} />
             </div>
             {/* <p className='font-kurale text-sm md:text-lg'>Robert Toth-Ludanyi 2024</p> */}
-            <a href="" className='
+            <a
+                href="#"
+                onClick={e => e.preventDefault()}
+                className='
             text-md font-play font-medium text-center border-2 p-2
             rounded-full border-transparent
             hover:border-oasis-600 hover:border-2'>
